75e66b15f1c3fa0cb6fd6ad4618565c0d2aebae1
VelFleX <121911776+VelFleX@users.noreply.github.com>
2023-11-16 18:36:15 +0300

    Delete js/global/beta.js
    Удалён из-за ненадобности

--- js/global/beta.js
+++
@@ -1,9 +0,0 @@
-let wasOpen = false;
-if (!wasOpen) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    if (tabs.length > 0) {
-      chrome.tabs.create({ url: 'html/popup.html' });
-    }
-  });
-  wasOpen = true;
-}
